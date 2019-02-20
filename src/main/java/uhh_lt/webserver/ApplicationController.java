9a18a9c62e1d4a7f84f0447dc3aa94b8ec09bee8
5meimers <bm_1@gmx.de>
2019-02-20 15:28:48 +0100

    vermieterTerms hinzugefügt

--- src/main/java/uhh_lt/webserver/ApplicationController.java
+++ src/main/java/uhh_lt/webserver/ApplicationController.java
@@ -87,7 +87,13 @@ public class ApplicationController {
             }
         }
 
-        float mieterAnteil = mieterScore / (mieterScore + vermieterScore);
+        for(String key : vermieterTerms.keySet() ) {
+            if (text.contains(key)) {
+                vermieterScore += vermieterTerms.get(key);
+            }
+        }
+
+        float mieterAnteil = (float)mieterScore / (mieterScore + vermieterScore);
 
         if (mieterAnteil > 0.5) {
             return "Analyse: Mieter " + mieterAnteil;
