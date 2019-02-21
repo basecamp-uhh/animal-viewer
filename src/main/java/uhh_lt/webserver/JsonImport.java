d704526ffc07f312425b8f23d3cfb1d253b088fe
jansen <jsntj@hotmail.com>
2019-02-21 15:28:50 +0100

    Create JsonImport - JsonRead ersezt

--- src/main/java/uhh_lt/webserver/JsonImport.java
+++ src/main/java/uhh_lt/webserver/JsonImport.java
@@ -2,11 +2,20 @@ package uhh_lt.webserver;
 import java.io.*;
 import org.json.*;
 import org.json.JSONObject;
-
 import org.json.JSONArray;
-
+//Data [
+//{
+//"Topic_id"
+//"T_Date"
+//"T_Subject"
+//"T_Price":
+//"T_Message
+//"T_Summary"
+//"R_posted
+//"R_Message"
+//}]
 /**
- *
+ * Import json
  ** **/
 public class JsonImport {
 
@@ -21,12 +30,11 @@ public class JsonImport {
         for (int i = 0; i < arr.length(); i++)
         {
             String topic_id = arr.getJSONObject(i).getString("Topic_id");
-            System.out.print("ID:"+""+ topic_id);
-            String topic_Message = arr.getJSONObject(i).getString("T_Message");
-            String Reply_Message = arr.getJSONObject(i).getString("R_Message");#
-            String topic_Message = arr.getJSONObject(i).getString("T_Message");
-            String topic_Message = arr.getJSONObject(i).getString("T_Message");
-            String topic_Message = arr.getJSONObject(i).getString("T_Message");
+            String topic_date = arr.getJSONObject(i).getString("T_Date");
+            String topic_subject = arr.getJSONObject(i).getString("T_Subject");
+            String topic_price= arr.getJSONObject(i).getString("T_Price");
+            String topic_message= arr.getJSONObject(i).getString("T_Message");
+            String topic_summary = arr.getJSONObject(i).getString("T_Summary");
 
 
         }
