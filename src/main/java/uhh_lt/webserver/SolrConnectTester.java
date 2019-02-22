76706139cd17ddc693a3eee369928cbd5a732323
5meimers <bm_1@gmx.de>
2019-02-22 10:29:39 +0100

    Solr Connector

--- src/main/java/uhh_lt/webserver/SolrConnectTester.java
+++ src/main/java/uhh_lt/webserver/SolrConnectTester.java
@@ -1,10 +1,12 @@
+package uhh_lt.webserver;
+
 import org.json.*;
 import uhh_lt.webserver.SolrConnect;
 
 public class SolrConnectTester {
 
     public static void main(String[] args) {
-        SolrConnect connect;
+        SolrConnect connect = new SolrConnect();
 
 
         JSONObject obj = new JSONObject("{\"T_date\": \"12039230921039213\"}");
