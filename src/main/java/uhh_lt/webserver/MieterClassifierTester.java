3e44e9ab99aaf9c77b964360fcd83d4fac3eb0c2
6hauptvo <n.holand@gmx.de>
2019-02-26 16:19:19 +0100

    Vorherige Änderungen rückgängig gemacht

--- src/main/java/uhh_lt/webserver/MieterClassifierTester.java
+++ src/main/java/uhh_lt/webserver/MieterClassifierTester.java
@@ -2,9 +2,10 @@ package uhh_lt.webserver;
 
 public class MieterClassifierTester
 {
-    public static void main(String[] args)
+    public static void nain(String[] args)
     {
         MieterClassifier MC = new MieterClassifier();
         System.out.println(MC.classify("Ich bin Vermieter."));
     }
+
 }
