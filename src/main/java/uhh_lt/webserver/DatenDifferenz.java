19838758de016099160ea413b60673c2328fc17c
6hauptvo <n.holand@gmx.de>
2019-02-26 14:08:22 +0100

    Eine Klasse zur Berechnung der Differenz von Daten mit ein paar Updates

--- src/main/java/uhh_lt/webserver/DatenDifferenz.java
+++ src/main/java/uhh_lt/webserver/DatenDifferenz.java
@@ -1,14 +1,22 @@
 package uhh_lt.webserver;
 
+import java.text.ParseException;
 import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class DatenDifferenz {
+
+
 
     public static void main(String[] args) {
 
         GregorianCalendar adate = new GregorianCalendar();
         GregorianCalendar qdate = new GregorianCalendar();
         printDifference(adate, qdate);
+
+        Differenz();
     }
 
     private static void printDifference(GregorianCalendar adate, GregorianCalendar qdate)
@@ -28,5 +36,28 @@ public class DatenDifferenz {
          minutes + " minutes, " +
          seconds + " seconds and " +
          millis + " milliseonds");*/
+    }
+
+    public static void Differenz()
+    {
+        try {
+            String sDate1 = "31/12/1998";
+            String sDate2 = "31/12/1999";
+            Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+            Date date2 = new SimpleDateFormat("dd/MM/yyyy").parse(sDate2);
+
+            long diffInMillies = Math.abs(date2.getTime() - date1.getTime());
+            long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+
+            System.out.println(sDate1 + "\t" + date1);
+            System.out.println(diff);
+        }
+
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
 }
