ff8957c5aff38951949ed502a26b3a24112aa766
jansen <jsntj@hotmail.com>
2019-02-26 11:04:22 +0100

    Create JsonImport - JsonRead ersezt

--- src/main/java/uhh_lt/webserver/SolrConnectTester.java
+++ src/main/java/uhh_lt/webserver/SolrConnectTester.java
@@ -4,16 +4,17 @@ import org.json.simple.JSONObject;
 //import org.json.*;
 import uhh_lt.webserver.SolrConnect;
 
+import java.io.IOException;
+
 public class SolrConnectTester {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SolrConnect connect = new SolrConnect();
-
+        connect.IdSearch();
 
         //JSONObject obj = new JSONObject();//"{\"T_Date\": \"12039230921039213\", \"Topic_id\": \"123\", \"T_Subject\": \"hallo du\", \"T_Price\": \"654341\", \"T_Message\": \"gfhshkz\", \"T_Summary\": \"gfhfghjghjfhtg\", \"R_posted\": \"fgdhgfhgdfhfg\", \"R_Message\": \"jdfghkjfgd\"}");
         //connect.store(obj);
 
 
-        connect.search("hallo");
-    }
+   }
 }
