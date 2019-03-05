68d50121b260b242f640e3e67d371ff13fd4e6c7
5meimers <bm_1@gmx.de>
2019-03-05 14:51:29 +0100

    Solr Connector

---
+++ src/main/java/uhh_lt/classifier/ClassifierInterface.java
@@ -0,0 +1,20 @@
+package uhh_lt.classifier;
+
+public interface ClassifierInterface
+{
+    Double classify(String text);
+
+    /**
+     * Gibt "true" zurück, wenn es sich um einen Mieter handelt, sonst "false"
+     * @return true wenn Mieter, false wenn Vermieter
+     */
+    boolean istMieterWarnGewerblich();
+
+    /**
+     * Wenn es sich mit einer Wahrscheinlichkeit von über 50% um einen Mieter handelt wird "true" ausgegeben, bei unter
+     * 50% "false" und bei genau 50% "unknown"
+     * @param text Ein String
+     * @return true wenn Mieter, false wenn Vermieter
+     */
+    Object istMieterWarnGewerblich(String text);
+}
